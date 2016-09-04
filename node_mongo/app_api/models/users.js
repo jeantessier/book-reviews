1be447f19bcf708c408b751ba915376ec47fac23
Jean Tessier <jean@directly.com>
2016-09-04 14:35:52 -0700

    Add JWT support to user model.  * npm install jsonwebtoken --save  * npm install dotenv --save

--- node_mongo/app_api/models/users.js
+++ node_mongo/app_api/models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
     email: {
@@ -23,6 +24,18 @@ userSchema.methods.setPassword = function(password) {
 userSchema.methods.validPassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
     return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiry.getTime() / 1000),
+    }, process.env.JWT_SECRET);
 };
 
 mongoose.model('User', userSchema);
