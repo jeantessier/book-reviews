679fd3e9eb5afbcf94dfaa19bc7a7896aea1797d
Jean Tessier <jean@directly.com>
2016-09-04 14:50:58 -0700

    Install and configure Passport to manage authentication.  * npm install passport --save  * npm install passport-local --save

--- node_mongo/app.js
+++ node_mongo/app.js
@@ -5,8 +5,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 require('./app_api/models/db');
+require('./app_api/config/passport.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
