b25be3403607703d4e93ab769d8193bee93dcd99
Jean Tessier <jean@directly.com>
2016-09-04 14:07:22 -0700

    Add database shutdown hooks.

--- node_mongo/app_api/models/db.js
+++ node_mongo/app_api/models/db.js
@@ -14,3 +14,26 @@ mongoose.connection.on("error", function(err) {
 mongoose.connection.on("disconnected", function() {
     console.log("Mongoose disconnected");
 });
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+var gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+// BRING IN YOUR SCHEMAS & MODELS
