c040e2b113a2f7b07cfa0e9887a76174ed93dd08
Jean Tessier <jean@jeantessier.com>
2019-11-04 15:11:05 -0800

    Add Devise controllers and routes
    Following instructions in [this article](https://medium.com/@nandhae/2019-how-i-set-up-authentication-with-jwt-in-just-a-few-lines-of-code-with-rails-5-api-devise-9db7d3cee2c0).
    
    I tried to register a new user, but the controller is not seeing the
    `params`.
    
        $ http --print hHbB :3000/signup Accept:application/json email=jean@jeantessier.com password=abcd1234
        POST /signup HTTP/1.1
        Accept: application/json
        Accept-Encoding: gzip, deflate
        Connection: keep-alive
        Content-Length: 57
        Content-Type: application/json
        Host: localhost:3000
        User-Agent: HTTPie/1.0.3
    
        {
            "email": "jean@jeantessier.com",
            "password": "abcd1234"
        }
    
        HTTP/1.1 422 Unprocessable Entity
        Cache-Control: no-cache
        Content-Type: application/json; charset=utf-8
        Transfer-Encoding: chunked
        Vary: Origin
        X-Request-Id: 01419273-6679-4e36-8550-52889b291caa
        X-Runtime: 0.130921
    
        {
            "errors": {
                "email": [
                    "can't be blank"
                ],
                "password": [
                    "can't be blank"
                ]
            }
        }

--- rails_mysql/config/routes.rb
+++ rails_mysql/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Override Devise's default routes: /users/sign_in, etc.
+  devise_for :users,
+             path: '',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             }
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
