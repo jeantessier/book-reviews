f80ef5499c5591836f5516d04427988b193664c3
Jean Tessier <jean@jeantessier.com>
2019-11-05 10:02:53 -0800

    Add dotenv suport
        $ bundle install

--- rails_mysql/config/initializers/devise.rb
+++ rails_mysql/config/initializers/devise.rb
@@ -302,7 +302,7 @@ Devise.setup do |config|
 
   # Configuration for devise-jwt
   config.jwt do |jwt|
-    jwt.secret = ENV['DEVISE_SECRET_KEY']
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
 
     jwt.dispatch_requests = [
         ['POST', %r{^/login$}]
