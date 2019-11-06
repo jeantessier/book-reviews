9479390eabcd544772bdadf78fd28ea2433f1290
Jean Tessier <jean@jeantessier.com>
2019-11-06 10:24:17 -0800

    Only authenticated users can modify /books

--- rails_mysql/app/controllers/books_controller.rb
+++ rails_mysql/app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user, only: [:create, :update, :destroy]
   before_action :set_book, only: [:show, :update, :destroy]
 
   # GET /books
