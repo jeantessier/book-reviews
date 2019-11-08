008176b1749d4a6191788123a733802e7ac60fb5
Jean Tessier <jean@jeantessier.com>
2019-11-08 11:39:35 -0800

    Fix model tests
    Added validation for the presence of `BookAuthor#name` and
    `BookYear#year`.

--- rails_mysql/app/models/book_author.rb
+++ rails_mysql/app/models/book_author.rb
@@ -1,5 +1,6 @@
 class BookAuthor < ApplicationRecord
   belongs_to :book, inverse_of: :authors
 
+  validates_presence_of :name
   validates_numericality_of :order, greater_than_or_equal_to: 0
 end
--- rails_mysql/app/models/book_year.rb
+++ rails_mysql/app/models/book_year.rb
@@ -1,5 +1,6 @@
 class BookYear < ApplicationRecord
   belongs_to :book, inverse_of: :authors
 
+  validates_presence_of :year
   validates_numericality_of :order, greater_than_or_equal_to: 0
 end
