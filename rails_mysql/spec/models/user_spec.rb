112bb6324d9095c93bc8ee3ff45e71b9c774d010
Jean Tessier <jean@jeantessier.com>
2019-11-11 11:37:36 -0800

    Fix User model tests for style

--- rails_mysql/spec/models/user_spec.rb
+++ rails_mysql/spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
   let(:email) { "email-#{rand 1_000...10_000}@test.com" }
   let(:password) { "password #{rand 1_000...10_000}" }
 
-  context "" do
-    subject { User.new email: email, password: password }
+  subject { User.new email: email, password: password }
 
-    it "should have an email address and password" do
-      expect(subject.email).to eq(email)
-    end
+  it "should have an email address and password" do
+    expect(subject.email).to eq(email)
+  end
 
+  context ".save" do
     it "should not save without an email" do
       user = User.new password: password
       expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid)
