2939538740f26cb7eee8073b6b5cde205e52b741
Jean Tessier <jean@jeantessier.com>
2019-12-27 15:34:32 -0800

    Flesh out gateway app
    Use `dotenv` to get `ENGINE_API_KEY` from the `.env` file.
    
    ```bash
    $ npm install dotenv
    ```
    
    Copied `index.js` from another example and tweaked `serviceList`.

---
+++ gateway/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require("@apollo/gateway");
+
+require('dotenv').config();
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'books', url: 'http://localhost:4001' },
+    { name: 'reviews', url: 'http://localhost:4002' },
+    { name: 'users', url: 'http://localhost:4003' },
+    { name: 'search', url: 'http://localhost:4004' },
+  ]
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+  engine: { apiKey: process.env.ENGINE_API_KEY },
+
+  // Disable subscriptions (not currently supported with ApolloGateway)
+  subscriptions: false,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
