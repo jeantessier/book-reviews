5ac03097838d346880c4cc6fde62926adb0810f1
Jean Tessier <jean@jeantessier.com>
2020-11-10 11:37:39 -0800

    Remove validation for flexibility
    The validation rules only work for a single association name.  If we try
    to batch load multiple associations and/or sub-associations, the
    validation fails because it only looks for a single association on the
    model itself.

--- rails_mysql/lib/graphql/batch_loaders/association_loader.rb
+++ rails_mysql/lib/graphql/batch_loaders/association_loader.rb
@@ -11,12 +11,10 @@ module GraphQL
       def initialize(model, association_name)
         @model = model
         @association_name = association_name
-        validate
       end
 
       def load(record)
         raise TypeError, "#{@model} loader can't load association for #{record.class}" unless record.is_a?(@model)
-        return Promise.resolve(read_association(record)) if association_loaded?(record)
         super
       end
 
@@ -32,22 +30,8 @@ module GraphQL
 
       private
 
-      def validate
-        unless @model.reflect_on_association(@association_name)
-          raise ArgumentError, "No association #{@association_name} on #{@model}"
-        end
-      end
-
       def preload_association(records)
         ::ActiveRecord::Associations::Preloader.new.preload(records, @association_name)
-      end
-
-      def read_association(record)
-        record.public_send(@association_name)
-      end
-
-      def association_loaded?(record)
-        record.association(@association_name).loaded?
       end
     end
   end
