3835a6c4c448ab4463cc76882dcca17a5f7e7a8b
Jean Tessier <jean@jeantessier.com>
2020-11-10 11:34:11 -0800

    Add inverse_of on associations for consistency

--- rails_mysql/app/models/review.rb
+++ rails_mysql/app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :reviewer, class_name: User.to_s
-  belongs_to :book
+  belongs_to :reviewer, inverse_of: :reviews, class_name: User.to_s
+  belongs_to :book, inverse_of: :reviews
 
   validates_presence_of :body
 end
