e19e724657c12081fdc161b553e3c2dcf4ab563c
Jean Tessier <jean@jeantessier.com>
2020-11-29 15:26:20 -0800

    Add Book entity and matching controller

---
+++ micronaut_neo4j/src/main/groovy/com/jeantessier/Book.groovy
@@ -0,0 +1,30 @@
+package com.jeantessier
+
+import grails.gorm.annotation.Entity
+
+@Entity
+class Book {
+
+    String id
+
+    String name
+    String publisher
+
+    static hasMany = [
+            authors: String,
+            years: String,
+    ]
+
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+        id maxSize: 36 // Gets assigned when we create them
+        name blank: false, unique: true
+    }
+
+    static mapping = {
+        id generator: "assigned", name: "id"
+    }
+
+}
---
+++ micronaut_neo4j/src/main/groovy/com/jeantessier/BookController.groovy
@@ -0,0 +1,76 @@
+package com.jeantessier
+
+import grails.gorm.transactions.Transactional
+import io.micronaut.http.annotation.*
+
+import java.text.MessageFormat
+
+@Controller("/books")
+class BookController {
+
+    @Get("/")
+    @Transactional(readOnly = true)
+    def index() {
+        Book.list()
+    }
+
+    @Get("/{id}")
+    @Transactional(readOnly = true)
+    def show(id) {
+        Book.get(id)
+    }
+
+    @Post("/")
+    @Transactional
+    def save(String name, String publisher, List<String> authors, List<String> years) {
+        def book = new Book(id: UUID.randomUUID() as String, name: name, publisher: publisher, authors: authors, years: years)
+        if (book.save()) {
+            return book
+        } else {
+            return book.errors.allErrors.collect { MessageFormat.format(it.defaultMessage, it.arguments) }.join(", ")
+        }
+    }
+
+    @Patch("/{id}")
+    @Transactional
+    def update(id, Optional<String> name, Optional<String> publisher, Optional<List<String>> authors, Optional<List<String>> years) {
+        def book = Book.get(id)
+        if (name.present) {
+            book.name = name.get()
+        }
+        if (publisher.present) {
+            book.publisher = publisher.get()
+        }
+        if (authors.present) {
+            book.authors = authors.get()
+        }
+        if (years.present) {
+            book.years = years.get()
+        }
+        if (book.save()) {
+            return book
+        } else {
+            return book.errors.allErrors.collect { MessageFormat.format(it.defaultMessage, it.arguments) }.join(", ")
+        }
+    }
+
+    @Delete("/")
+    @Transactional
+    def delete() {
+        def numberDeleted = Book.executeUpdate("DELETE FROM book")
+        return "Deleted ${numberDeleted} records."
+    }
+
+    @Delete("/{id}")
+    @Transactional
+    def delete(id) {
+        def book = Book.get(id)
+        if (book) {
+            book.delete()
+            return "OK"
+        } else {
+            return "Could not delete book ${id}."
+        }
+    }
+
+}
