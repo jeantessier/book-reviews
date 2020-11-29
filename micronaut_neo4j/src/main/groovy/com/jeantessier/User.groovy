09bedd479dc8b8c33b28e6c748241d88c7505e22
Jean Tessier <jean@jeantessier.com>
2020-11-29 14:21:45 -0800

    Add simple entity and controller
    I had to downgrade Neo4j from `4.2` to `3.5` because the ORM is using a
    CYPHER syntax that is no longer valid.  It was deprecated in `3.0` and
    removed in `4.0`.  I don't quite get why the drivers have not been
    updated.
    
    I can `GET /users` when there are no `User` nodes, but the moment I
    create one, it starts to `500` with this error message coming from the
    driver:
    
    ```
    Cannot access records on this result any more as the result has already
    been consumed or the query runner where the result is created has
    already been closed.
    ```
    
    I can `POST /users` and `GET /users/<id>` and `DELETE /users/<id>` just
    fine.

---
+++ micronaut_neo4j/src/main/groovy/com/jeantessier/User.groovy
@@ -0,0 +1,28 @@
+package com.jeantessier
+
+import grails.gorm.annotation.*
+
+@Entity
+class User {
+
+//    String id
+
+    String email
+    String password
+    String name
+
+//    Date dateCreated
+//    Date lastUpdated
+
+    static constraints = {
+//        id maxSize: 36, nullable: true // Gets assigned automatically by the mapping generator
+        email email: true
+        password nullable: true
+        name blank: false
+    }
+
+//    static mapping = {
+//        id generator: "uuid2"
+//    }
+
+}
---
+++ micronaut_neo4j/src/main/groovy/com/jeantessier/UserController.groovy
@@ -0,0 +1,72 @@
+package com.jeantessier
+
+import grails.gorm.transactions.Transactional
+import io.micronaut.http.annotation.*
+import java.text.*
+
+@Controller("/users")
+class UserController {
+
+    @Get("/")
+    @Transactional(readOnly = true)
+    def index() {
+        User.list()
+    }
+
+    @Get("/{id}")
+    @Transactional(readOnly = true)
+    def show(id) {
+        User.get(id)
+    }
+
+    @Post("/")
+    @Transactional
+    def save(String email, String password, String name) {
+        def user = new User(email: email, password: password, name: name)
+        if (user.save()) {
+            return user
+        } else {
+            return user.errors.allErrors.collect { MessageFormat.format(it.defaultMessage, it.arguments) }.join(", ")
+        }
+    }
+
+    @Patch("/{id}")
+    @Transactional
+    def update(id, Optional<String> email, Optional<String> password, Optional<String> name) {
+        def user = User.get(id)
+        if (email.present) {
+            user.email = email.get()
+        }
+        if (password.present) {
+            user.password = password.get()
+        }
+        if (name.present) {
+            user.name = name.get()
+        }
+        if (user.save()) {
+            return user
+        } else {
+            return user.errors.allErrors.collect { MessageFormat.format(it.defaultMessage, it.arguments) }.join(", ")
+        }
+    }
+
+    @Delete("/")
+    @Transactional
+    def delete() {
+        def numberDeleted = User.executeUpdate("DELETE FROM user")
+        return "Deleted ${numberDeleted} records."
+    }
+
+    @Delete("/{id}")
+    @Transactional
+    def delete(id) {
+        def user = User.get(id)
+        if (user) {
+            user.delete()
+            return "OK"
+        } else {
+            return "Could not delete user ${id}."
+        }
+    }
+
+}
