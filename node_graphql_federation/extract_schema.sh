ef0bf5794a5a0571b8398c1c5c3293378551e47b
Jean Tessier <jean@jeantessier.com>
2021-05-25 18:10:26 -0700

    Refactor extract_schema to use Bash
    Simulates an associative array that works with older versions of Bash,
    like the one in current MacOS.

---
+++ node_graphql_federation/extract_schema.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+readonly SERVICES=(
+    books:4001
+    reviews:4002
+    users:4003
+    search:4004
+    signatures:4005
+)
+
+for service in ${SERVICES[*]}
+do
+    echo '==========' ${service%%:*} '=========='
+    rover subgraph introspect http://localhost:${service#*:} > ${service%%:*}/subgraph.graphql
+done
+
+echo '==========' supergraph '=========='
+rover supergraph compose --config ./supergraph-config.yml > supergraph.graphql
