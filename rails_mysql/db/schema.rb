4057d9b8ed138a6e486e95089a0f65c3b1a9c92c
Jean Tessier <jean@jeantessier.com>
2021-05-29 15:24:56 -0700

    Remove DELETE CASCADE in the database
    Let Rails deal with associations, so it can call the appropriate
    callbacks when deleting entities.  It is less efficient than handling
    associations in the database with a single `DELETE` statement, but it is
    better from a business logic perspective.
    
    I hadn't regenerated `schema.rb` in a while and newer versions of Rails
    structure it slightly differently.  The relevant changes are on lines 73
    and 74.

--- rails_mysql/db/schema.rb
+++ rails_mysql/db/schema.rb
@@ -2,24 +2,24 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115061855) do
+ActiveRecord::Schema.define(version: 2019_11_15_061855) do
 
-  create_table "book_authors", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "book_authors", id: :integer, charset: "utf8", force: :cascade do |t|
     t.integer "book_id"
     t.string "author", null: false
     t.integer "order", default: 0, null: false
     t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
-  create_table "book_titles", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "book_titles", id: :integer, charset: "utf8", force: :cascade do |t|
     t.integer "book_id"
     t.string "title", null: false
     t.string "link", limit: 1023
@@ -27,14 +27,14 @@ ActiveRecord::Schema.define(version: 20191115061855) do
     t.index ["book_id"], name: "index_book_titles_on_book_id"
   end
 
-  create_table "book_years", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "book_years", id: :integer, charset: "utf8", force: :cascade do |t|
     t.integer "book_id"
     t.string "year", null: false
     t.integer "order", default: 0, null: false
     t.index ["book_id"], name: "index_book_years_on_book_id"
   end
 
-  create_table "books", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "books", id: :integer, charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "publisher"
     t.datetime "created_at", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20191115061855) do
     t.index ["name"], name: "index_books_on_name", unique: true
   end
 
-  create_table "reviews", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "reviews", id: :integer, charset: "utf8", force: :cascade do |t|
     t.integer "reviewer_id"
     t.integer "book_id"
     t.text "body", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20191115061855) do
     t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
   end
 
-  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", id: :integer, charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -70,6 +70,6 @@ ActiveRecord::Schema.define(version: 20191115061855) do
   add_foreign_key "book_authors", "books"
   add_foreign_key "book_titles", "books"
   add_foreign_key "book_years", "books"
-  add_foreign_key "reviews", "books", on_delete: :cascade
-  add_foreign_key "reviews", "users", column: "reviewer_id", on_delete: :cascade
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users", column: "reviewer_id"
 end
