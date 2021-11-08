fb1ac4e4905fc8695a7932df8ac8adf0b0e933fb
Jean Tessier <jean@jeantessier.com>
2021-11-08 14:33:28 -0800

    Install the apollo-federation gem

--- rails_graphql_federation/books/app/graphql/types/base_interface.rb
+++ rails_graphql_federation/books/app/graphql/types/base_interface.rb
@@ -1,6 +1,8 @@
 module Types
   module BaseInterface
     include GraphQL::Schema::Interface
+    include ApolloFederation::Interface
+
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
 
