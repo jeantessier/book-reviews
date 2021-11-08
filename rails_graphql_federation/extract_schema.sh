66e5627fafaab502a9408a37018b996e875af545
Jean Tessier <jean@jeantessier.com>
2021-11-08 15:14:00 -0800

    Add script to extract federated graph
    Copied the script from `../node_graphql_federation`.

---
+++ rails_graphql_federation/extract_schema.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+readonly SERVICES=(
+    books:3001
+)
+
+for service in ${SERVICES[*]}
+do
+    echo '==========' ${service%%:*} '=========='
+    rover subgraph introspect http://localhost:${service#*:}/graphql > ${service%%:*}/subgraph.graphql
+done
+
+echo '==========' supergraph '=========='
+rover supergraph compose --config ./supergraph-config.yml > supergraph.graphql
