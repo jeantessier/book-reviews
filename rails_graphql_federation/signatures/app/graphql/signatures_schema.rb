2bb78c626ea772e90589243b260345d3f753dc88
Jean Tessier <jean@jeantessier.com>
2021-11-09 10:49:16 -0800

    Install the apollo-federation gem

--- rails_graphql_federation/signatures/app/graphql/signatures_schema.rb
+++ rails_graphql_federation/signatures/app/graphql/signatures_schema.rb
@@ -1,4 +1,6 @@
 class SignaturesSchema < GraphQL::Schema
+  include ApolloFederation::Schema
+
   mutation(Types::MutationType)
   query(Types::QueryType)
 
--- rails_graphql_federation/signatures/app/graphql/types/base_field.rb
+++ rails_graphql_federation/signatures/app/graphql/types/base_field.rb
@@ -1,5 +1,7 @@
 module Types
   class BaseField < GraphQL::Schema::Field
+    include ApolloFederation::Field
+
     argument_class Types::BaseArgument
   end
 end
