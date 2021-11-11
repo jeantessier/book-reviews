b21c6119ef466d5736ed0476de61abe4a9509f0a
Jean Tessier <jean@jeantessier.com>
2021-11-11 00:39:06 -0800

    Resolve cross-service entity references for reviews

--- rails_graphql_federation/reviews/app/graphql/types/review_type.rb
+++ rails_graphql_federation/reviews/app/graphql/types/review_type.rb
@@ -8,5 +8,9 @@ module Types
     field :stop, String, null: true
 
     key fields: 'id'
+
+    def self.resolve_reference(reference, _context)
+      ReviewRepository.find_by_id(reference[:id])
+    end
   end
 end
