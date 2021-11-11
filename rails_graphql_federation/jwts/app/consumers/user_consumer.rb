6f551072da9d4c2b25973498857691b9e54bf99e
Jean Tessier <jean@jeantessier.com>
2021-11-11 02:45:06 -0800

    Consume Kakfa messages to manage state

---
+++ rails_graphql_federation/jwts/app/consumers/user_consumer.rb
@@ -0,0 +1,51 @@
+class UserConsumer
+  include Phobos::Handler
+
+  def consume(payload, metadata)
+    Rails.logger.info "#{self.class.name} #{metadata[:topic]}[#{metadata[:partition]}] offset: #{metadata[:offset]}, headers: #{metadata[:headers]}, key: #{metadata[:key]}, value: #{payload}"
+
+    json_message = JSON.parse(payload)
+    message_type = json_message.delete('type')
+
+    Rails.logger.info "#{message_type} #{json_message}"
+
+    case message_type
+    when 'userAdded'
+      add_user json_message
+    when 'userUpdated'
+      update_user json_message
+    when 'userRemoved'
+      remove_user json_message
+    else
+      Rails.logger.info "Skipping ..."
+    end
+  end
+
+  private
+
+  def add_user(json_message)
+    user = {
+      id: json_message['id'],
+      name: json_message['name'],
+      email: json_message['email'],
+      password: json_message['password'],
+      roles: json_message['roles'],
+    }
+
+    UserRepository.save(user)
+  end
+
+  def update_user(json_message)
+    user = UserRepository.find_by_id(json_message['id'])
+    raise "No user with ID #{json_message['id']}" if book.nil?
+
+    user[:name] = json_message['name'] if json_message.include?('name')
+    user[:email] = json_message['email'] if json_message.include?('email')
+    user[:password] = json_message['password'] if json_message.include?('password')
+    user[:roles] = json_message['roles'] if json_message.include?('roles')
+  end
+
+  def remove_user(json_message)
+    UserRepository.remove(json_message['id'])
+  end
+end
--- rails_graphql_federation/jwts/app/graphql/mutations/login.rb
+++ rails_graphql_federation/jwts/app/graphql/mutations/login.rb
@@ -6,7 +6,28 @@ module Mutations
     field :jwt, String, 'a signed JWT for the user with matching email and password', null: true
 
     def resolve(email:, password:)
-      { jwt: "" }
+      user = UserRepository.find_by_email(email)
+      raise "No user with email \"#{email}\"." if user.nil?
+
+      raise "Password for #{email} does not match." unless user[:password] == password
+
+      { jwt: generate_jwt(user) }
+    end
+
+    private
+
+    def generate_jwt(user)
+      payload = {
+        name: user[:name],
+        email: user[:email],
+        roles: user[:roles],
+      }
+      options = {
+        expiresIn: 3 * 24 * 60 *  60, # 3 days in seconds
+        issuer: 'book-reviews',
+        subject: user[:id],
+      }
+      payload.merge(options).to_json
     end
   end
 end
