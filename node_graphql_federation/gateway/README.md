47d5506dfb4d28ca209cb8dd71bd90c9117b7ebc
Jean Tessier <jean@jeantessier.com>
2022-12-11 20:49:53 -0800

    Upgrade the gateway to @apollo/server
    Use the latest `@apollo` packages.  Add some traces when using the
    `DEBUG` environment variable.
    
    Cf. https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup/

--- node_graphql_federation/gateway/README.md
+++ node_graphql_federation/gateway/README.md
@@ -1,4 +1,11 @@
-# Book Reviews &mdash; Node GraphQL Federation &mdash; Gateway
+# Book Reviews &mdash; Node GraphQL Federation &mdash; Gateway  
 
 A microservice for combining microservices using GraphQL federation as part of a
 book reviews application.
+
+Set the `DEBUG` environment variable for additional information on sub-queries
+to the various services.
+
+```bash
+DEBUG=1 npm start
+```
--- node_graphql_federation/gateway/index.js
+++ node_graphql_federation/gateway/index.js
@@ -1,4 +1,5 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
 const { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } = require("@apollo/gateway")
 
 require('dotenv').config()
@@ -10,6 +11,33 @@ const search_service = process.env.SEARCH_SERVICE || 'http://localhost:4004'
 const signatures_service = process.env.SIGNATURES_SERVICE || 'http://localhost:4005'
 const jwts_service = process.env.JWTS_SERVICE || 'http://localhost:4006'
 
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+    willSendRequest({ request, context }) {
+        if (context.authHeader) {
+            request.http.headers.set('Authorization', context.authHeader)
+        }
+        if (process.env.DEBUG) {
+            console.log("Will send request!")
+            console.log(`    context: ${JSON.stringify(context)}`)
+            console.log(`    request: ${JSON.stringify(request)}`)
+            console.log()
+        }
+    }
+
+    didReceiveResponse({ request, response, context }) {
+        if (process.env.DEBUG) {
+            console.log("Did receive response!")
+            console.log(`    context: ${JSON.stringify(context)}`)
+            console.log(`    request: ${JSON.stringify(request)}`)
+            console.log(`    response: ${JSON.stringify(response)}`)
+            console.log()
+        }
+        return response
+    }
+}
+
+// See https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup/
+// for why IntrospectAndCompose is not recommended for production systems.
 const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
@@ -21,37 +49,26 @@ const gateway = new ApolloGateway({
             { name: 'jwts', url: jwts_service },
         ],
     }),
-    buildService: ({ url, name }) => {
-        return new(class extends RemoteGraphQLDataSource {
-            willSendRequest({ request, context }) {
-                if (context.authHeader) {
-                    request.http.headers.set('Authorization', context.authHeader)
-                }
-            }
-        })({ url, name })
+    buildService: ({ name, url }) => {
+        return new AuthenticatedDataSource({ url })
     }
 })
 
 // Pass the ApolloGateway to the ApolloServer constructor
 const server = new ApolloServer({
     gateway,
-
-    // Disable subscriptions (not currently supported with ApolloGateway)
-    subscriptions: false,
-    context: ({ req }) => {
-        const authHeader = req.headers.authorization || ''
-        if (!authHeader) return {}
-
-        return { authHeader }
-    },
     plugins: [
         {
             requestDidStart(requestContext) {
                 console.log(`====================   ${new Date().toJSON()}   ====================`)
                 console.log("Request did start!")
+                if (process.env.DEBUG)  {
+                    console.log(`    context: ${JSON.stringify(requestContext.contextValue)}`)
+                }
                 console.log(`    query: ${requestContext.request.query}`)
                 console.log(`    operationName: ${requestContext.request.operationName}`)
                 console.log(`    variables: ${JSON.stringify(requestContext.request.variables)}`)
+                console.log()
             },
         },
     ],
@@ -59,6 +76,14 @@ const server = new ApolloServer({
 
 const port = process.env.PORT || 4000
 
-server.listen(port).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
+startStandaloneServer(server, {
+    context: async ({ req }) => {
+        const authHeader = req.headers.authorization || ''
+        if (!authHeader) return {}
+
+        return { authHeader }
+    },
+    listen: { port },
+}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
 })
