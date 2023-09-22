bf34320bc97e9c1d49b2793ee88590b3c971b06f
Jean Tessier <jean@jeantessier.com>
2023-09-21 18:02:14 -0700

    Code cleanup

--- node_mongo/app_api/config/passport.js
+++ node_mongo/app_api/config/passport.js
@@ -6,18 +6,16 @@ const User = mongoose.model('User')
 passport.use(new LocalStrategy(
     { usernameField: 'email' },
     (username, password, done) => {
-        User.findOne({
-            email: username
-        }).then(user => {
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' })
-            }
-            if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect password.' })
-            }
-            return done(null, user)
-        }).catch(err => {
-            return done(err)
-        })
+        User
+            .findOne({ email: username })
+            .then(user => {
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' })
+                }
+                if (!user.validPassword(password)) {
+                    return done(null, false, { message: 'Incorrect password.' })
+                }
+                return done(null, user)
+            }).catch(err => done(err))
     }
 ))
