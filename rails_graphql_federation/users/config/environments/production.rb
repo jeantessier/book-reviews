97128e5ef3729933313e04d185d0a860ad5fa9c2
Jean Tessier <jean@jeantessier.com>
2024-01-10 17:36:40 -0800

    Turn off ActiveRecord
    Just as if I had created the Rails applications with `--skip-active-record`.

--- rails_graphql_federation/users/config/environments/production.rb
+++ rails_graphql_federation/users/config/environments/production.rb
@@ -30,9 +30,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
@@ -53,7 +50,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "users_production"
+  # config.active_job.queue_name_prefix = "books_production"
 
   config.action_mailer.perform_caching = false
 
@@ -80,7 +77,4 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
