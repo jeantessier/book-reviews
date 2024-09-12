3f84ca7c8d2a685a001b2127bd94bee56615808e
Jean Tessier <jean@jeantessier.com>
2024-09-12 00:57:16 -0700

    Don't index quotes and dashes

--- node_graphql_federation/search/index.js
+++ node_graphql_federation/search/index.js
@@ -197,7 +197,7 @@ const scrubIndex = (word, id) => {
     }
 }
 
-const normalize = text => text.replace(/[!?.&]/g, '')
+const normalize = text => text.replace(/[!?.&]/g, '').replace(/['"-]/, ' ')
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
