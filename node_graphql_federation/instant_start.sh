cbf3d21a973f2d6a9e54de7cfef7a5191784cd98
Jean Tessier <jean@jeantessier.com>
2025-02-09 16:29:31 -0800

    List possible image modes in Docker Compose scripts

--- node_graphql_federation/instant_start.sh
+++ node_graphql_federation/instant_start.sh
@@ -8,6 +8,11 @@ if [[ ! -r $COMPOSE_FILE ]]
 then
     echo Unknown mode: $IMAGE_MODE
     echo Compose file $COMPOSE_FILE not found
+    echo Available choices:
+    for f in $(ls docker-compose*.yml | perl -n -e 'print $1 . "\n" if /docker-compose.(.*).yml/ && $1 !~ /override/')
+    do
+      echo "    " $f
+    done
     exit 1
 fi
 
